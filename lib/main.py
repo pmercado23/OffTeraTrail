f1e9a3076399f35c5f948588d9c52ea1dc821843
pmercado23 <pmercado@ucsc.edu>
2019-11-15 14:51:32 -0500

    updating with running loop

--- lib/main.py
+++ lib/main.py
@@ -1,39 +1,71 @@
-import parts.core as core
-import parts.member as memeber
 from random import randint
 
-PLAY_DATA = None
+
+class CoreData:
+    def __init__(self, months, funding_cost, motivation, player_data):
+        self.months = months
+        self.funding_cost = funding_cost
+        self.motivation = motivation
+        self.loss = False
+        self.player_data = player_data
+
+
+class Player:
+    def __init__(self, founders, team, funding, options):
+        self.founders = founders
+        self.team = team
+        self.funding = funding
+        self.options = options
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
 
 
 def load_data():
-    global PLAY_DATA
-    PLAY_DATA = core.Core([memeber.Member("bill", 22), memeber.Member("bob", 22)],
-                          [memeber.Member("suraj", 22), memeber.Member("pierre", 22)],
-                          100,
+    return Player([Person("harry"), Person("dan")],
+                          [Person("suraj"), Person("pierre")],
+                          100000,
                           100)
 
-def month_pass(months_current):
-    months_left = months_current-1
-    event = roll_d(20)
-    print("a month has passed, {} months remain, event {} has occurred".format(months_left, event))
 
+def month_pass(game_data):
+    game_data.months -= 1
+    event = roll_d(20)
+    print("a month has passed, {} months remain, event {} has occurred".format(game_data.months, event))
+    funding_decrease(game_data, 50)
+    print("you have {} left in funding".format(game_data.player_data.funding))
+
+
+
+def funding_decrease(game_data, costs):
+    game_data.player_data.funding -= costs
+
+
+def check_loss(game_data):
+    if (game_data.player_data.funding <= 0 or game_data.months <= 0):
+        game_data.loss = True
 
 
 def roll_d(x):
     return randint(0, x)
 
 
-def main_loop(months, player_data):
-    print("gameplay loop here with {} months".format(months))
-    month_pass(months)
+def game_loop(game_data):
+    while (not game_data.loss):
+        print("gameplay loop here with {} months".format(game_data.months))
+        month_pass(game_data)
+        check_loss(game_data)
 
 
 
 def main():
-    load_data()
-    months = 60
+    player_data = load_data()
+    game_data = CoreData(60, 10, 100, player_data)
     print("Starting....")
-    main_loop(months, PLAY_DATA)
+    game_loop(game_data)
 
 
 if __name__=="__main__":
