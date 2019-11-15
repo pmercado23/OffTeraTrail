649fa02587757e5e755437249c4da9ea789b65f9
pmercado23 <pmercado@ucsc.edu>
2019-11-15 16:04:42 -0500

    basic loop with decreeing funding

--- lib/main.py
+++ lib/main.py
@@ -1,5 +1,4 @@
-from random import randint
-
+from parts import gameplay_loop
 
 class CoreData:
     def __init__(self, months, funding_cost, motivation, player_data):
@@ -31,41 +30,12 @@ def load_data():
                           100)
 
 
-def month_pass(game_data):
-    game_data.months -= 1
-    event = roll_d(20)
-    print("a month has passed, {} months remain, event {} has occurred".format(game_data.months, event))
-    funding_decrease(game_data, 50)
-    print("you have {} left in funding".format(game_data.player_data.funding))
-
-
-
-def funding_decrease(game_data, costs):
-    game_data.player_data.funding -= costs
-
-
-def check_loss(game_data):
-    if (game_data.player_data.funding <= 0 or game_data.months <= 0):
-        game_data.loss = True
-
-
-def roll_d(x):
-    return randint(0, x)
-
-
-def game_loop(game_data):
-    while (not game_data.loss):
-        print("gameplay loop here with {} months".format(game_data.months))
-        month_pass(game_data)
-        check_loss(game_data)
-
-
 
 def main():
     player_data = load_data()
     game_data = CoreData(60, 10, 100, player_data)
     print("Starting....")
-    game_loop(game_data)
+    gameplay_loop.main_game_loop(game_data)
 
 
 if __name__=="__main__":
